1944cdb82bfc80ad4068a62598f4640bda73dca6
Laureano Oliva <laureanoliva@advantaseeds.com>
2025-11-19 16:04:28 -0300

    amount difference colum removal

--- Querys/amountDifference.sql
+++ Querys/amountDifference.sql
@@ -3,12 +3,6 @@ SELECT
     "Cust. Code",
     "Order Amount",
     "Importe Neto",
-    "ZCP1 Condition Type Value",
-    "Total Value",
-    "ZK18 Condition Type Value",
-    "Rejected Qty",
-    "Billing Date",
-    "Sales Document Type",
     ABS("Importe Neto" - "Total Value") AS Difference_ImporteNeto_TotalValue,
 FROM joined_sap_sfdc
 WHERE (ABS("Importe Neto" - "Total Value") > 10);
